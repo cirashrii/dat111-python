6bacfa614d3953c66c949051a7d47cf8987664ca
RagnhildSG <Ragnhild2002@hotmail.com>
2025-10-27 14:21:20 +0100

    import

--- PythonFile_Semesteroppgave/Semesteroppgave2025.py
+++ PythonFile_Semesteroppgave/Semesteroppgave2025.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import image as mpimg
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
